21e5e45b09d88c275d504df8c04638e1157019f3
kartavyaKloud <kartavya@kloudone.com>
2024-01-19 23:12:23 +0530

    onclick now passing the id

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,17 +1,19 @@
 import React, { useState } from 'react'
 import Card from "../components/shared/Card"
+import {FaTimes} from 'react-icons/fa'
 
 const FeedbackList = (props) => {
     if(!props.feedback || props.feedback.length === 0) {
       return (<p>No feedback yet</p>)
     }
-    const handleClick = ()  => {
-      console.log(1)
+    const handleClick = (id)  => {
+      console.log(id)
     }
   return (
     <div>
       <Card reverse = {true}>
         <div className="num-display">{props.feedback.rating}</div>
+        <button onClick ={() => handleClick(props.feedback.id)}><FaTimes/></button>
         <div className="text-display">{props.feedback.text}</div>
         <button onClick={handleClick}>Change Rating</button>
       </Card>
