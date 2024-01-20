dd940ed5ea4acface55c08c0436d7b5607814391
kartavyaKloud <kartavya@kloudone.com>
2024-01-20 15:14:01 +0530

    Delete Functionality Working now was immediately calling the deleteFeedback function previously

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,35 @@
-import './App.css'
-import Header from './components/Header.jsx'
-import FeedbackList from './components/FeedbackList.jsx'
-import { useState } from 'react'
-import FeedbackData from './utils/FeedbackData.js'
-import Card from './components/shared/Card.jsx'
+import "./App.css";
+import Header from "./components/Header.jsx";
+import FeedbackList from "./components/FeedbackList.jsx";
+import { useState } from "react";
+import FeedbackData from "./utils/FeedbackData.js";
+import Card from "./components/shared/Card.jsx";
 
 function App() {
-  const [feedback , setFeedback] = useState(FeedbackData)
+  const [feedback, setFeedback] = useState(FeedbackData);
+  const deleteFeedback = (id) => {
+    if(window.confirm("Are you sure want to delete")) {
+      console.log("Deleting feedback with ID:", id);
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
   return (
     <>
-    <Header text = "Hello World"/>
-    <div className="container">
-    {FeedbackData.map((feedback) => {
-      return (<FeedbackList key = {feedback.id} feedback={feedback}/>)
-    })}
-    <Card reverse={false}>
-    Hello World
-    </Card>
-    </div>
+      <Header text="Hello World" />
+      <div className="container">
+        {feedback.map((feedback) => {
+          return (
+            <FeedbackList
+              key={feedback.id}
+              feedback={feedback}
+              deleteFeedback={(id) => deleteFeedback(id)}
+            />
+          );
+        })}
+        <Card reverse={false}>Hello World</Card>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
