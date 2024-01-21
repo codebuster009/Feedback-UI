f9e99c700668f673bc02e476d1f7dcaf35eb8965
kartavyaKloud <kartavya@kloudone.com>
2024-01-22 00:56:47 +0530

    custom button

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react'
 import Card from './shared/Card'
+import Button from './shared/Button'
 const FeedbackForm = () => {
   const [text , setText] = useState("")
+  const [disabled , setdisabled] = useState(true)
   return (
     <Card>
       <form>
         <input type="text" onChange={(e) => {setText(e.target.value)}} placeholder = "Type your name" value={text}/>
-        <button type='submit'>Send</button>
+        <Button type='submit' version = 'primary'>Send</Button>
       </form>
     </Card>
   )
