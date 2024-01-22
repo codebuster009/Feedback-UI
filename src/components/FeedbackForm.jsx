dfcb331e0cab3282433184df7e6acdb51940bd5b
kartavyaKloud <kartavya@kloudone.com>
2024-01-22 23:39:55 +0530

    Validating the button and disabling it if input is less than 10 characters

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,11 +4,22 @@ import Button from './shared/Button'
 const FeedbackForm = () => {
   const [text , setText] = useState("")
   const [disabled , setdisabled] = useState(true)
+  const [message , setMessage] = useState(null)
+  function handleText(e) {
+    if(text === "" || text.trim().length <= 10) {
+      setdisabled(true)
+      setMessage("Text must be atleast 10 characters")
+    } else{
+      setdisabled(false)
+    }
+    setText(e.target.value)
+  }
   return (
     <Card>
       <form>
-        <input type="text" onChange={(e) => {setText(e.target.value)}} placeholder = "Type your name" value={text}/>
-        <Button type='submit' version = 'primary'>Send</Button>
+        <input type="text" onChange={handleText} placeholder = "Type your name" value={text}/>
+        {message && message}
+        <Button type='submit' version = 'primary' disabled={disabled}>Send</Button>
       </form>
     </Card>
   )
--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const Button = ({ type, children, version }) => {
+const Button = ({ type, children, version , disabled}) => {
     console.log(type)
     console.log(version)
     console.log(children)
+    console.log(disabled)
   return (
-    <button type={type}  className={`btn btn-${version}`}>
+    <button type={type}  className={`btn btn-${version}`} disabled = {disabled}>
 {children}
 </button>
   );
