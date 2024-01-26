fdaa1a577963c6cd91fdb04596bcf1024242045d
kartavyaKloud <kartavya@kloudone.com>
2024-01-26 18:06:20 +0530

    Submitting the form

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react'
 import Card from './shared/Card'
 import Button from './shared/Button'
+import RatingComponent from './RatingComponent'
 const FeedbackForm = () => {
   const [text , setText] = useState("")
   const [disabled , setdisabled] = useState(true)
   const [message , setMessage] = useState(null)
+  const [rating , setRating] = useState(1)
   function handleText(e) {
     if(text.trim().length <= 10) {
       setdisabled(true)
@@ -17,11 +19,26 @@ const FeedbackForm = () => {
     }
     setText(e.target.value)
   }
+  function handleSubmit(e){
+    e.preventDefault()
+    if(text.trim().length > 10) {
+      const newFeedback = {
+        text,
+        rating
+      }
+      console.log(newFeedback)
+    }
+  }
   return (
     <Card>
-      <form>
+      <form onClick={handleSubmit}>
         <input type="text" onChange={handleText} placeholder = "Type your name" value={text}/>
         {message && message}
+        <Card>
+      <div className="rating-parent">
+      <RatingComponent select = {(rating) => {setRating(rating)}}/>
+      </div>
+      </Card>
         <Button type='submit' version = 'primary' disabled={disabled}>Send</Button>
       </form>
     </Card>
--- src/components/RatingComponent.jsx
+++ src/components/RatingComponent.jsx
@@ -1,27 +1,30 @@
 import React, { useState } from 'react';
 
-const RatingComponent = () => {
+const RatingComponent = ({select}) => {
   const [selected, setSelected] = useState(1);
 
-  const handleRatingChange = (e) => {
-    setSelected(Number(e.target.value));
+  const handleClick = (e) => {
+    console.log(e.currentTarget.value);
+    setSelected(Number(e.currentTarget.value));
+    select(Number(e.currentTarget.value) + 1)
   };
 
   return (
     <>
       <ul>
-      {[...Array(11).keys()].map((i)=>(
-        <li key={i}>
-        <input 
-        type="radio"
-        name = "rating"
-        id = {`num${i}`}
-        value={i}
-        checked = {selected === selected}
-         />
-         <label htmlFor={`num${i}`}>{i}</label>
-        </li>
-      ))}
+        {[...Array(11).keys()].map((i) => (
+          <li key={i}>
+            <input
+              type="radio"
+              name="rating"
+              id={`num${i + 1}`}
+              value={i} 
+              checked={selected === i}
+              onChange={handleClick}
+            />
+            <label htmlFor={`num${i + 1}`}>{i + 1}</label>
+          </li>
+        ))}
       </ul>
       <div>Rating Select</div>
     </>
