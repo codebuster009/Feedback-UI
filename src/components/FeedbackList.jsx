3bcc098438b098f2af0a7725b9697993019d9f7e
kartavyaKloud <kartavya@kloudone.com>
2024-01-27 23:50:22 +0530

    editing the reviews

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,11 +1,11 @@
 import React, { useContext } from 'react';
 import { motion, AnimatePresence } from 'framer-motion'; // Import motion and AnimatePresence
 import Card from '../components/shared/Card';
-import { FaTimes } from 'react-icons/fa';
+import { FaEdit, FaTimes } from 'react-icons/fa';
 import FeedbackContext from '../context/FeedbackContext.jsx';
 
 const FeedbackList = ({ newFeedback }) => {
-    const { feedback, deleteFeedback } = useContext(FeedbackContext);
+    const { feedback, deleteFeedback , editFeedback } = useContext(FeedbackContext);
 
     if (!feedback || feedback.length === 0) {
         return <p>No feedback yet</p>;
@@ -24,6 +24,9 @@ const FeedbackList = ({ newFeedback }) => {
                         <div className="num-display">{item.rating}</div>
                         <button onClick={() => deleteFeedback(item.id)}>
                             <FaTimes />
+                        </button>
+                        <button style={{ backgroundColor: 'transparent', border: 'none' }}>
+                            <FaEdit color='purple' onClick={() => editFeedback(item)}/>
                         </button>
                         <div className="text-display">{item.text}</div>
                     </Card>
